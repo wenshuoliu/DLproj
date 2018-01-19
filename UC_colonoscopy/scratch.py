3994d42fb68e4cb94076da89278185405595a032
wenshuoliu <wenshuoliu@gmail.com>
2018-01-19 09:44:31 -0500

    Added the code to train model on multi-GPUs, in order to use high resolution input images. Added all the callbacks to control the training process.

--- UC_colonoscopy/scratch.py
+++ UC_colonoscopy/scratch.py
@@ -6,12 +6,16 @@ from keras.layers.core import Dense, Activation, Dropout, Lambda, Flatten
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.optimizers import SGD, RMSprop, Adam
 from keras.preprocessing.image import ImageDataGenerator
-from keras.utils import to_categorical, plot_model
+from keras.utils import multi_gpu_model
+import tensorflow as tf
+from keras.callbacks import Callback, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+import pickle
 
 path = "/nfs/turbo/intmed-bnallamo-turbo/wsliu/Data/UC_colonoscopy/"
 model_path = path + 'models/'
 
-batch_size=64
+batch_size=4
+G = 4 # number of GPU's
 
 train_datagen = ImageDataGenerator( 
         rotation_range=90,
@@ -26,46 +30,57 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
         path+'splitted/train/',  # this is the target directory
-        target_size=(256, 320),  
-        batch_size=batch_size,
+        target_size=(512, 640),  
+        batch_size=batch_size*G,
         class_mode='categorical')
 
 validation_generator = test_datagen.flow_from_directory(
         path+'splitted/validation/',
-        target_size=(256, 320),
-        batch_size=batch_size,
+        target_size=(512, 640),
+        batch_size=batch_size*G,
+        shuffle=False,
         class_mode='categorical')
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(256, 320, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2), strides=(2,2)))
+with tf.device('/cpu:0'):
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=(512, 640, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2,2)))
 
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2), strides=(2,2)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2,2)))
 
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2), strides=(2,2)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2,2)))
 
-model.add(Conv2D(256, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-model.add(Flatten())  
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(4))
-model.add(Activation('softmax'))
+    model.add(Flatten())  
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4))
+    model.add(Activation('softmax'))
+    
+parallel_model = multi_gpu_model(model, gpus=G)
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
+adam = Adam()
+parallel_model.compile(loss='categorical_crossentropy',
+              optimizer=adam,
               metrics=['accuracy'])
 
-model.fit_generator(
-        train_generator,
-        steps_per_epoch=train_generator.samples // batch_size,
-        epochs=50,
-        validation_data=validation_generator,
-        validation_steps=validation_generator.samples // batch_size,
-        verbose=2);
+checkpointer = ModelCheckpoint(filepath=model_path+'scratch_hd0118.h5', verbose=0, save_best_only=True, save_weights_only=True)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, min_lr=1.e-7)
+earlystop = EarlyStopping(patience=30)
 
-model.save_weights(model_path+'scratch1.h5')
+history = parallel_model.fit_generator(
+            train_generator,
+            steps_per_epoch=train_generator.samples // (batch_size*G),
+            epochs=300,
+            validation_data=validation_generator,
+            validation_steps=validation_generator.samples // (batch_size*G),
+            callbacks = [checkpointer, reduce_lr, earlystop],
+            verbose=2);
+
+with open('history_hd0118.pkl', 'wb') as f:
+    pickle.dump(history.history, f, -1)
