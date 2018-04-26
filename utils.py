3364aa095408b990088e22d29d0d920e0b459ae0
wenshuoliu <wenshuoliu@gmail.com>
2018-04-25 22:25:24 -0400

    Finished a naive version of MLP for the prediction of 30-day readmission of the ami cohort. It is essentially the same  as the result of mixed effects logistic regression.

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from itertools import cycle
 from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 
-def plot_roc(y_pred, y_true):
+def plot_roc(y_pred, y_true, classes=None):
     """This function plot the ROC curve and return the AUC"""
     if len(y_pred.shape)==1:
         y_pred = y_pred.reshape(y_pred.shape+(1,))
@@ -18,7 +18,13 @@ def plot_roc(y_pred, y_true):
         fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_pred[:, i])
         roc_auc[i] = auc(fpr[i], tpr[i])
     colors = cycle(['aqua', 'darkorange', 'cornflowerblue', 'navy'])
-    legends = ['class'+str(j+1) for j in range(n_classes)]
+    
+    if classes is None:
+        legends = ['class'+str(j+1) for j in range(n_classes)]
+    elif len(classes == n_classes):
+        legends = classes
+    else:
+        print("Number of classes doesn't match labels")
     for i, color in zip(range(n_classes), colors):
         plt.plot(fpr[i], tpr[i], color=color,
                  label='ROC curve of {0} (area = {1:0.2f})'
