96e60cb13d08a82c99cafafe391e83a084b675b3
wenshuoliu <wenshuoliu@gmail.com>
2018-05-02 14:56:13 -0400

    Finished the class FrameIterator and ImageFrameGenerator to generate mini batches with labels from a pandas DataFrame.

--- NRD/hyper_tune.py
+++ NRD/hyper_tune.py
@@ -35,13 +35,17 @@ depths = [1, 3, 5, 10, 20, 50]
 widths = [64, 128, 256, 512, 1024]
 DX_dims = [5, 10, 20, 50]
 hosp_dims = [1, 3, 5, 10]
+lrs = [0.001, 0.0005, 0.0002, 0.0001]
+dropouts = [0.5, 0.6, 0.7, 0.8, 0.9]
+batch_sizes = [512, 256, 128, 64]
 
 n_sample = 100
 parameters = list(zip(np.random.choice(depths, n_sample), np.random.choice(widths, n_sample),
-                       np.random.choice(DX_dims, n_sample), np.random.choice(hosp_dims, n_sample)))
+                       np.random.choice(DX_dims, n_sample), np.random.choice(hosp_dims, n_sample),
+                     np.random.choice(lrs, n_sample), np.random.choice(dropouts, n_sample), np.random.choice(batch_sizes, n_sample)))
 parameters = [p for p in parameters if parameters.count(p)==1]
-param_tried = pd.read_csv('./hyper_tune.csv')
-param_tried = param_tried[['depth', 'width', 'DX_dim', 'hosp_dim']].values
+param_tried = pd.read_csv('output/hyper_tune_large.csv')
+param_tried = param_tried[['depth', 'width', 'DX_dim', 'hosp_dim', 'lr', 'dropout', 'batchsize']].values
 param_tried = [tuple(p) for p in list(param_tried)]
 
 def sc_block(input_tensor, width):
@@ -61,7 +65,7 @@ def sc_block(input_tensor, width):
     x = Activation('relu')(x)
     return x
 
-def model_build(depth, width, DX_dim, hosp_dim):
+def model_build(depth, width, DX_dim, hosp_dim, lr, dropout, batchsize):
     input_DX = Input(shape = (29,))
     DX_embed = Embedding(input_dim=n_DX_cat, output_dim=DX_dim, input_length=29)(input_DX)
     DX_embed = Reshape((29*DX_dim,))(DX_embed)
@@ -80,34 +84,35 @@ def model_build(depth, width, DX_dim, hosp_dim):
         x = sc_block(x, width)
 
     x = Dense(width, activation='relu')(x)
-    x = Dropout(0.5)(x)
+    x = Dropout(dropout)(x)
     x = Dense(width, activation='relu')(x)
     prediction = Dense(2, activation='softmax')(x)
     model = Model(inputs=[input_demo, input_DX1, input_DX, input_hosp], outputs=prediction)
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    adam = Adam(lr=lr)
+    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 class_weight = {0:(y_train.shape[0]/sum(y_train[:, 0])), 1:(y_train.shape[0]/sum(y_train[:, 1]))}
-adam = Adam(lr=0.0002)
 
 #with open('./hyper_tune.csv', 'a') as f:
 #    f.write('depth,width,DX_dim,hosp_dim,auc\n')
 for param in parameters:
     if not param in param_tried: 
         print("Training with parameters: ", *param)
+        batch_size = param[6]
         model = model_build(*param)
-        checkpointer = ModelCheckpoint(filepath=model_path+'ami_icd9_{}_{}_{}_{}.h5'.format(*param), verbose=0, save_best_only=True, save_weights_only=True)
+        checkpointer = ModelCheckpoint(filepath=model_path+'ami_icd9_{}_{}_{}_{}_{}_{}_{}.h5'.format(*param), verbose=0, save_best_only=True, save_weights_only=True)
         reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=1.e-8)
         earlystop = EarlyStopping(monitor='val_loss', patience=20)
         hist = model.fit([demo_mat[:N_train, :], DX1_mat[:N_train, :], DX_mat[:N_train, :], hosp_array[:N_train]], y_train, 
-                         batch_size=256, epochs=100, callbacks=[checkpointer, reduce_lr, earlystop], class_weight=class_weight, 
+                         batch_size=batch_size, epochs=150, callbacks=[checkpointer, reduce_lr, earlystop], class_weight=class_weight, 
                          validation_data=[[demo_mat[N_train:, :], DX1_mat[N_train:, :], DX_mat[N_train:, :], hosp_array[N_train:]], y_test], 
                          verbose=2)
-        model.load_weights(model_path+'ami_icd9_{}_{}_{}_{}.h5'.format(*param))
-        y_pred = model.predict([demo_mat[N_train:, :], DX1_mat[N_train:, :], DX_mat[N_train:, :], hosp_array[N_train:]], batch_size=256)
+        model.load_weights(model_path+'ami_icd9_{}_{}_{}_{}_{}_{}_{}.h5'.format(*param))
+        y_pred = model.predict([demo_mat[N_train:, :], DX1_mat[N_train:, :], DX_mat[N_train:, :], hosp_array[N_train:]], batch_size=batch_size)
         fpr, tpr, _ = roc_curve(y_test[:, 0], y_pred[:, 0])
         roc_auc = auc(fpr, tpr)
-        with open('./hyper_tune.csv', 'a') as f:
-            f.write('{0},{1},{2},{3},{4:.3f}\n'.format(*param,roc_auc))
+        with open('output/hyper_tune_large.csv', 'a') as f:
+            f.write('{0},{1},{2},{3},{4},{5},{6},{7:.3f}\n'.format(*param,roc_auc))
         del(model)
 
--- keras_addon.py
+++ keras_addon.py
@@ -1,6 +1,7 @@
 #### Some addons for keras ----
 
 from keras.preprocessing.image import *  
+from keras.preprocessing.image import _count_valid_files_in_directory
 
 class FrameIterator(Iterator):
     """Iterator capable of reading images from a directory on disk, and labels 
@@ -32,9 +33,9 @@ class FrameIterator(Iterator):
     def __init__(self, directory, dataframe, file_names, labels, image_data_generator,
                  target_size=(256, 256), color_mode='rgb',
                  batch_size=32, shuffle=True, seed=None,
-                 data_format=None,
+                 data_format=None, save_to_dir=None, save_prefix='', 
+                 save_format='png',
                  follow_links=False,
-                 subset=None,
                  interpolation='nearest'):
         if data_format is None:
             data_format = K.image_data_format()
@@ -56,6 +57,7 @@ class FrameIterator(Iterator):
                 self.image_shape = self.target_size + (1,)
             else:
                 self.image_shape = (1,) + self.target_size
+        '''
         self.classes = classes
         if class_mode not in {'categorical', 'binary', 'sparse',
                               'input', None}:
@@ -63,8 +65,12 @@ class FrameIterator(Iterator):
                              '; expected one of "categorical", '
                              '"binary", "sparse", "input"'
                              ' or None.')
+        '''
+        self.save_to_dir = save_to_dir
+        self.save_prefix = save_prefix
+        self.save_format = save_format
         self.interpolation = interpolation
-
+        '''
         if subset is not None:
             validation_split = self.image_data_generator._validation_split
             if subset == 'validation':
@@ -77,44 +83,34 @@ class FrameIterator(Iterator):
         else:
             split = None
         self.subset = subset
-
-        white_list_formats = {'png', 'jpg', 'jpeg', 'bmp', 'ppm', 'tif', 'tiff'}
+        '''
+        white_list_formats = {'png', 'jpg', 'jpeg', 'bmp', 'ppm', 'tif', 'tiff', 'npy'}
 
         # first, count the number of samples and classes
         self.samples = 0
-
-        '''
-        if not classes:
-            classes = []
-            for subdir in sorted(os.listdir(directory)):
-                if os.path.isdir(os.path.join(directory, subdir)):
-                    classes.append(subdir)
-        self.num_classes = len(classes)
-        self.class_indices = dict(zip(classes, range(len(classes))))
-        '''
-        self.samples = _count_valid_files_in_directory(self.directory, white_list_formats, split, follow_links)
+        self.samples = _count_valid_files_in_directory(self.directory, white_list_formats, follow_links)
 
         print('Found %d images.' % (self.samples))
 
-        # second, build an index of the images in the different class subfolders
-        classes, filenames = _list_valid_filenames_in_directory(directory, white_list_formats, split, 
+        filenames = [f for f in os.listdir(directory) if not os.path.isdir(os.path.join(directory, f))]
+       
+        ext = set()
+        for f in filenames:
+            for e in white_list_formats:
+                if f.lower().endswith('.'+e):
+                    ext.add(e)
+        if len(ext)>1:
+            print("Image files must have the same extension.")
+        ext = ext.pop()
+            
+        sub_df = dataframe[[file_names]+labels]
+        sub_df = sub_df.set_index(file_names, drop=True)
+        sub_df.index = [i+'.'+ext for i in sub_df.index]
+        filenames = sorted([fn for fn in filenames if fn in sub_df.index])
+        self.filenames = np.array(filenames)
+        self.labels = sub_df.loc[filenames,:]
         
-        self.filenames = []
-        self.classes = np.zeros((self.samples,), dtype='int32')
-        i = 0
-        for dirpath in (os.path.join(directory, subdir) for subdir in classes):
-            results.append(pool.apply_async(_list_valid_filenames_in_directory,
-                                            (dirpath, white_list_formats, split,
-                                             self.class_indices, follow_links)))
-        for res in results:
-            classes, filenames = res.get()
-            self.classes[i:i + len(classes)] = classes
-            self.filenames += filenames
-            i += len(classes)
-
-        pool.close()
-        pool.join()
-        super(DirectoryIterator, self).__init__(self.samples, batch_size, shuffle, seed)
+        super(FrameIterator, self).__init__(self.samples, batch_size, shuffle, seed)
 
     def _get_batches_of_transformed_samples(self, index_array):
         batch_x = np.zeros((len(index_array),) + self.image_shape, dtype=K.floatx())
@@ -140,18 +136,13 @@ class FrameIterator(Iterator):
                                                                   format=self.save_format)
                 img.save(os.path.join(self.save_to_dir, fname))
         # build batch of labels
-        if self.class_mode == 'input':
-            batch_y = batch_x.copy()
-        elif self.class_mode == 'sparse':
-            batch_y = self.classes[index_array]
-        elif self.class_mode == 'binary':
-            batch_y = self.classes[index_array].astype(K.floatx())
-        elif self.class_mode == 'categorical':
-            batch_y = np.zeros((len(batch_x), self.num_classes), dtype=K.floatx())
-            for i, label in enumerate(self.classes[index_array]):
-                batch_y[i, label] = 1.
-        else:
-            return batch_x
+        label_dim = self.labels.shape[1]
+        label_names = self.labels.columns
+        batch_y = dict()
+        for l in label_names:
+            batch_y[l] = self.labels.loc[self.filenames[index_array], l].values
+            
+        
         return batch_x, batch_y
 
     def next(self):
@@ -164,3 +155,108 @@ class FrameIterator(Iterator):
         # The transformation of images is not under thread lock
         # so it can be done in parallel
         return self._get_batches_of_transformed_samples(index_array)
+    
+    
+
+class ImageFrameGenerator(ImageDataGenerator):
+    """Extension of ImageDataGenerator to generate batches with labels from 
+    a pandas DataFrame. 
+    # Arguments
+        featurewise_center: set input mean to 0 over the dataset.
+        samplewise_center: set each sample mean to 0.
+        featurewise_std_normalization: divide inputs by std of the dataset.
+        samplewise_std_normalization: divide each input by its std.
+        zca_whitening: apply ZCA whitening.
+        zca_epsilon: epsilon for ZCA whitening. Default is 1e-6.
+        rotation_range: degrees (0 to 180).
+        width_shift_range: fraction of total width, if < 1, or pixels if >= 1.
+        height_shift_range: fraction of total height, if < 1, or pixels if >= 1.
+        shear_range: shear intensity (shear angle in degrees).
+        zoom_range: amount of zoom. if scalar z, zoom will be randomly picked
+            in the range [1-z, 1+z]. A sequence of two can be passed instead
+            to select this range.
+        channel_shift_range: shift range for each channel.
+        fill_mode: points outside the boundaries are filled according to the
+            given mode ('constant', 'nearest', 'reflect' or 'wrap'). Default
+            is 'nearest'.
+            Points outside the boundaries of the input are filled according to the given mode:
+                'constant': kkkkkkkk|abcd|kkkkkkkk (cval=k)
+                'nearest':  aaaaaaaa|abcd|dddddddd
+                'reflect':  abcddcba|abcd|dcbaabcd
+                'wrap':  abcdabcd|abcd|abcdabcd
+        cval: value used for points outside the boundaries when fill_mode is
+            'constant'. Default is 0.
+        horizontal_flip: whether to randomly flip images horizontally.
+        vertical_flip: whether to randomly flip images vertically.
+        rescale: rescaling factor. If None or 0, no rescaling is applied,
+            otherwise we multiply the data by the value provided. This is
+            applied after the `preprocessing_function` (if any provided)
+            but before any other transformation.
+        preprocessing_function: function that will be implied on each input.
+            The function will run before any other modification on it.
+            The function should take one argument:
+            one image (Numpy tensor with rank 3),
+            and should output a Numpy tensor with the same shape.
+        data_format: 'channels_first' or 'channels_last'. In 'channels_first' mode, the channels dimension
+            (the depth) is at index 1, in 'channels_last' mode it is at index 3.
+            It defaults to the `image_data_format` value found in your
+            Keras config file at `~/.keras/keras.json`.
+            If you never set it, then it will be "channels_last".
+    """    
+    def __init__(self,
+                 featurewise_center=False,
+                 samplewise_center=False,
+                 featurewise_std_normalization=False,
+                 samplewise_std_normalization=False,
+                 zca_whitening=False,
+                 zca_epsilon=1e-6,
+                 rotation_range=0.,
+                 width_shift_range=0.,
+                 height_shift_range=0.,
+                 shear_range=0.,
+                 zoom_range=0.,
+                 channel_shift_range=0.,
+                 fill_mode='nearest',
+                 cval=0.,
+                 horizontal_flip=False,
+                 vertical_flip=False,
+                 rescale=None,
+                 preprocessing_function=None,
+                 data_format=None):
+        super(ImageFrameGenerator, self).__init__(featurewise_center=featurewise_center,
+                 samplewise_center=samplewise_center,
+                 featurewise_std_normalization=featurewise_std_normalization,
+                 samplewise_std_normalization=samplewise_std_normalization,
+                 zca_whitening=zca_whitening,
+                 zca_epsilon=zca_epsilon,
+                 rotation_range=rotation_range,
+                 width_shift_range=width_shift_range,
+                 height_shift_range=height_shift_range,
+                 shear_range=shear_range,
+                 zoom_range=zoom_range,
+                 channel_shift_range=channel_shift_range,
+                 fill_mode=fill_mode,
+                 cval=cval,
+                 horizontal_flip=horizontal_flip,
+                 vertical_flip=vertical_flip,
+                 rescale=rescale,
+                 preprocessing_function=preprocessing_function,
+                 data_format=data_format)
+        
+            
+    def flow_from_frame(self, directory, dataframe, file_names, labels,
+                 target_size=(256, 256), color_mode='rgb',
+                 batch_size=32, shuffle=True, seed=None,
+                 data_format=None, save_to_dir=None, save_prefix='', 
+                 save_format='png',
+                 follow_links=False,
+                 interpolation='nearest'):
+        return FrameIterator(directory, dataframe, file_names, labels, self,
+                 target_size=target_size, color_mode=color_mode,
+                 batch_size=batch_size, shuffle=shuffle, seed=seed,
+                 data_format=self.data_format, save_to_dir=save_to_dir, save_prefix=save_prefix, 
+                 save_format=save_format,
+                 follow_links=follow_links,
+                 interpolation=interpolation)
+            
+    
