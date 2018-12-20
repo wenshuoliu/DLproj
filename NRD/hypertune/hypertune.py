3dca0981ba126008468200bde38f180dae7841d8
wenshuoliu <wenshuoliu@gmail.com>
2018-12-20 16:37:45 -0500

    NRD: embedding DX1, DX, PR into different spaces with GloVe on subset of codes.

--- NRD/hypertune/hypertune.py
+++ NRD/hypertune/hypertune.py
@@ -22,17 +22,17 @@ tst_seeds = [0]
 cohorts = ['ami']
 #DX_rarecutpoints = [20]
 #PR_rarecutpoints = [drp/2 for drp in DX_rarecutpoints]
-val_folds = [5]
-lambs = [0.1, 1., 10.]
+val_seeds = [0, 1, 2]
+lambs = [0.2, 1., 10.]
 n_sampless = [100]
 result_files = ['output/ht_result1210_{}.csv']
 
 para_itr = itertools.product(model_names, code_embed_dims, fc_widths, md_widths, lr1s, dropouts, batchsizes, embed_mats, 
-                            tst_seeds, cohorts, val_folds, lambs, n_sampless, result_files)
-para_lst = [(mn, ced, fc, md, l1, do, bs, em, ts, ch, vf, la, ns, rf) 
-            for mn, ced, fc, md, l1, do, bs, em, ts, ch, vf, la, ns, rf in para_itr]
+                            tst_seeds, cohorts, val_seeds, lambs, n_sampless, result_files)
+para_lst = [(mn, ced, fc, md, l1, do, bs, em, ts, ch, vs, la, ns, rf) 
+            for mn, ced, fc, md, l1, do, bs, em, ts, ch, vs, la, ns, rf in para_itr]
 
 n_jobs = 10
 for para, job_ind in zip(para_lst, itertools.cycle(range(n_jobs))):
     with open('hypertune'+str(job_ind)+'.sh', 'a') as f:
-        f.write('python template_skipgram1209.py --model_name {} --code_embed_dim {} --fc_width {} --md_width {} --lr1 {} --dropout {} --batchsize {} --embed_file {} --tst_seed {} --cohort {} --n_val_folds {} --lamb {} --n_samples {} --result_file {} --job_index {}\n'.format(*para, job_ind))
+        f.write('python template_skipgram1209.py --model_name {} --code_embed_dim {} --fc_width {} --md_width {} --lr1 {} --dropout {} --batchsize {} --embed_file {} --tst_seed {} --cohort {} --val_seed {} --lamb {} --n_samples {} --result_file {} --job_index {}\n'.format(*para, job_ind))
--- NRD/hypertune/template_skipgram1209.py
+++ NRD/hypertune/template_skipgram1209.py
@@ -13,7 +13,7 @@ parser.add_argument('--batchsize', type=int, default=128)
 parser.add_argument('--embed_file', type=str, default='random')
 parser.add_argument('--cohort', type=str, default='ami')
 parser.add_argument('--tst_seed', type=int, default=0, help='the seed to split training/test data')
-parser.add_argument('--n_val_folds', type=int, default=10, help='number of folds to split training/validation data')
+parser.add_argument('--val_seed', type=int, default=0, help='see to split trn/val')
 parser.add_argument('--result_file', type=str, default='output/result.csv')
 #parser.add_argument('--penalty', type=float, default=0.5)
 #parser.add_argument('--penalty_metric', type=str, default='cosine')
@@ -37,7 +37,7 @@ batchsize = args.batchsize
 embed_file = args.embed_file
 cohort = args.cohort
 tst_seed = args.tst_seed
-n_val_folds = args.n_val_folds
+val_seed = args.val_seed
 result_file = args.result_file
 #penalty = args.penalty
 #penalty_metric = args.penalty_metric
@@ -51,7 +51,7 @@ job_index = args.job_index
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.metrics import roc_curve, auc
 import os, sys
 from datetime import datetime
@@ -144,117 +144,110 @@ else:
     embed_initializer = Constant(embed_mat)
 
 #split trn/val data, do a n_fold validation
-y_pred_lst = []
-auc_lst = []
-skf = StratifiedKFold(n_splits=n_val_folds, random_state=24, shuffle=True)
-for trn_idx, val_idx in skf.split(train_df, train_df.HOSP_NRD):
-    DX1_array_train = train_df['DX1'].values
-    DX1_array_trn = DX1_array_train[trn_idx]
-    DX1_array_val = DX1_array_train[val_idx]
+trn_df, val_df = train_test_split(train_df.reset_index(drop=True), test_size = 0.2, stratify=train_df.HOSP_NRD, random_state = val_seed)
+trn_idx = trn_df.index
+val_idx = val_df.index
 
-    DX_mat_train = train_df[DXs].values
-    DX_mat_trn = DX_mat_train[trn_idx, ]
-    DX_mat_val = DX_mat_train[val_idx, ]
+DX1_array_train = train_df['DX1'].values
+DX1_array_trn = DX1_array_train[trn_idx]
+DX1_array_val = DX1_array_train[val_idx]
 
-    PR_mat_train = train_df[PRs].values
-    PR_mat_trn = PR_mat_train[trn_idx, ]
-    PR_mat_val = PR_mat_train[val_idx, ]
+DX_mat_train = train_df[DXs].values
+DX_mat_trn = DX_mat_train[trn_idx, ]
+DX_mat_val = DX_mat_train[val_idx, ]
+
+PR_mat_train = train_df[PRs].values
+PR_mat_trn = PR_mat_train[trn_idx, ]
+PR_mat_val = PR_mat_train[val_idx, ]
     
-    hosp_array_train = train_df['HOSP_NRD'].values
-    hosp_array_trn = hosp_array_train[trn_idx]
-    hosp_array_val = hosp_array_train[val_idx]
+hosp_array_train = train_df['HOSP_NRD'].values
+hosp_array_trn = hosp_array_train[trn_idx]
+hosp_array_val = hosp_array_train[val_idx]
     
-    demo_mat_train = train_df[['AGE', 'FEMALE']].values
-    demo_mat_train[:, 0] = (demo_mat_train[:, 0]-age_mean)/age_std
-    pay1_mat_train = to_categorical(train_df.PAY1.values, num_classes=n_pay1)[:, 1:]
-    los_array_train = (train_df.LOS.values - los_mean)/los_std
-    ed_mat_train = to_categorical(train_df.HCUP_ED.values, num_classes=n_ed)
-    zipinc_mat_train = to_categorical(train_df.ZIPINC_QRTL.values, num_classes=n_zipinc)[:, 1:]
-    other_mat_train = np.concatenate((demo_mat_train, pay1_mat_train, los_array_train.reshape(los_array_train.shape+(1,)), 
+demo_mat_train = train_df[['AGE', 'FEMALE']].values
+demo_mat_train[:, 0] = (demo_mat_train[:, 0]-age_mean)/age_std
+pay1_mat_train = to_categorical(train_df.PAY1.values, num_classes=n_pay1)[:, 1:]
+los_array_train = (train_df.LOS.values - los_mean)/los_std
+ed_mat_train = to_categorical(train_df.HCUP_ED.values, num_classes=n_ed)
+zipinc_mat_train = to_categorical(train_df.ZIPINC_QRTL.values, num_classes=n_zipinc)[:, 1:]
+other_mat_train = np.concatenate((demo_mat_train, pay1_mat_train, los_array_train.reshape(los_array_train.shape+(1,)), 
                                     ed_mat_train, zipinc_mat_train), axis=1)
-    other_mat_trn = other_mat_train[trn_idx, ]
-    other_mat_val = other_mat_train[val_idx, ]
+other_mat_trn = other_mat_train[trn_idx, ]
+other_mat_val = other_mat_train[val_idx, ]
 
-    y_train = train_df.readm30.astype(int).values
-    y_trn = y_train[trn_idx]
-    y_val = y_train[val_idx]
-    Y_trn = to_categorical(y_train[trn_idx])
-    Y_val = to_categorical(y_train[val_idx])
+y_train = train_df.readm30.astype(int).values
+y_trn = y_train[trn_idx]
+y_val = y_train[val_idx]
+Y_trn = to_categorical(y_train[trn_idx])
+Y_val = to_categorical(y_train[val_idx])
     
-    labels_flatten_trn = labels_flatten_train[trn_idx, ]
-    labels_flatten_val = labels_flatten_train[val_idx, ]
-    conditions_trn = conditions_train[trn_idx, ]
-    conditions_val = conditions[val_idx, ]
+labels_flatten_trn = labels_flatten_train[trn_idx, ]
+labels_flatten_val = labels_flatten_train[val_idx, ]
+conditions_trn = conditions_train[trn_idx, ]
+conditions_val = conditions[val_idx, ]
     
-    # model building 
-    input_DX1 = Input(shape=(1,))
-    DX1_embed0 = Embedding(input_dim=n_code_cat, output_dim=code_embed_dim, embeddings_initializer=embed_initializer, 
+# model building 
+input_DX1 = Input(shape=(1,))
+DX1_embed0 = Embedding(input_dim=n_code_cat, output_dim=code_embed_dim, embeddings_initializer=embed_initializer, 
                           name='DX1_embed')(input_DX1)
-    DX1_embed = Reshape((code_embed_dim,))(DX1_embed0)
+DX1_embed = Reshape((code_embed_dim,))(DX1_embed0)
     
-    input_DX = Input(shape = (len(DXs),))
-    DX_embed0 = Embedding(input_dim=n_code_cat, output_dim=code_embed_dim, mask_zero=True, embeddings_initializer=embed_initializer, 
+input_DX = Input(shape = (len(DXs),))
+DX_embed0 = Embedding(input_dim=n_code_cat, output_dim=code_embed_dim, mask_zero=True, embeddings_initializer=embed_initializer, 
                          name='DX_embed')(input_DX)
-    DX_embed = MaskedDense(md_width, activation='relu')(DX_embed0)
-    DX_embed = MaskedSum()(DX_embed0)
+DX_embed = MaskedDense(md_width, activation='relu')(DX_embed0)
+DX_embed = MaskedSum()(DX_embed0)
     
-    input_PR = Input(shape = (len(PRs),))
-    PR_embed0 = Embedding(input_dim=n_code_cat, output_dim=code_embed_dim, mask_zero=True, embeddings_initializer=embed_initializer, 
+input_PR = Input(shape = (len(PRs),))
+PR_embed0 = Embedding(input_dim=n_code_cat, output_dim=code_embed_dim, mask_zero=True, embeddings_initializer=embed_initializer, 
                          name='PR_embed')(input_PR)
-    PR_embed = MaskedDense(md_width, activation='relu')(PR_embed0)
-    PR_embed = MaskedSum()(PR_embed0)
+PR_embed = MaskedDense(md_width, activation='relu')(PR_embed0)
+PR_embed = MaskedSum()(PR_embed0)
     
-    input_hosp = Input(shape=(1,))
-    hosp_embed = Embedding(input_dim=n_hosp_cat, output_dim=hosp_embed_dim, input_length=1)(input_hosp)
-    hosp_embed = Reshape((hosp_embed_dim, ))(hosp_embed)
-    input_other = Input(shape=(other_mat_train.shape[1], ))
+input_hosp = Input(shape=(1,))
+hosp_embed = Embedding(input_dim=n_hosp_cat, output_dim=hosp_embed_dim, input_length=1)(input_hosp)
+hosp_embed = Reshape((hosp_embed_dim, ))(hosp_embed)
+input_other = Input(shape=(other_mat_train.shape[1], ))
     
-    merged = Concatenate(axis=1)([DX1_embed, DX_embed, PR_embed, hosp_embed, input_other])
-    input_labels_flatten = Input(shape=(2025, ))
-    input_conditions = Input(shape=(2025, ))
-    cr =  SkipgramRegularization(n_samples, n_code_cat, lamb)([DX1_embed0, DX_embed0, PR_embed0, input_labels_flatten, 
+merged = Concatenate(axis=1)([DX1_embed, DX_embed, PR_embed, hosp_embed, input_other])
+input_labels_flatten = Input(shape=(2025, ))
+input_conditions = Input(shape=(2025, ))
+cr =  SkipgramRegularization(n_samples, n_code_cat, lamb)([DX1_embed0, DX_embed0, PR_embed0, input_labels_flatten, 
                                                                input_conditions])
     
-    merged = Dense(fc_width, activation='relu')(merged)
-    merged = Dropout(dropout)(merged)
-    prediction = Dense(2, activation='softmax', name='prediction')(merged)
-    model = Model(inputs=[input_DX1, input_DX, input_PR, input_hosp, input_other,input_labels_flatten,input_conditions], 
+merged = Dense(fc_width, activation='relu')(merged)
+merged = Dropout(dropout)(merged)
+prediction = Dense(2, activation='softmax', name='prediction')(merged)
+model = Model(inputs=[input_DX1, input_DX, input_PR, input_hosp, input_other,input_labels_flatten,input_conditions], 
                   outputs=[prediction,cr])  
     
-    adam = Adam(lr=lr1)
-    model.compile(optimizer=adam, loss=[K.binary_crossentropy,zero_loss])
+adam = Adam(lr=lr1)
+model.compile(optimizer=adam, loss=[K.binary_crossentropy,zero_loss])
     
-    auccheckpoint = AUCCheckPoint(filepath=model_path+'skipgram_temp'+str(job_index)+'.h5', 
+auccheckpoint = AUCCheckPoint(filepath=model_path+'skipgram_temp'+str(job_index)+'.h5', 
                                   validation_y=[Y_val, np.random.randn(Y_val.shape[0],1)], 
                                      validation_x=[DX1_array_val, DX_mat_val, PR_mat_val, hosp_array_val, other_mat_val, 
                                                    labels_flatten_val, conditions_val], auc_output_idx=[0])
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10, min_lr=K.epsilon())
-    earlystop = EarlyStopping(monitor='val_loss', patience=30)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10, min_lr=K.epsilon())
+earlystop = EarlyStopping(monitor='val_loss', patience=30)
     
-    class_weight = {0:(Y_trn.shape[0]/sum(Y_trn[:, 0])), 1:(Y_trn.shape[0]/sum(Y_trn[:, 1]))}
+class_weight = {0:(Y_trn.shape[0]/sum(Y_trn[:, 0])), 1:(Y_trn.shape[0]/sum(Y_trn[:, 1]))}
     
-    hist = model.fit([DX1_array_trn, DX_mat_trn, PR_mat_trn, hosp_array_trn, other_mat_trn, labels_flatten_trn, conditions_trn], 
+hist = model.fit([DX1_array_trn, DX_mat_trn, PR_mat_trn, hosp_array_trn, other_mat_trn, labels_flatten_trn, conditions_trn], 
                  [Y_trn, np.random.randn(Y_trn.shape[0], 1)], batch_size=batchsize, epochs=100, 
                  validation_data=[[DX1_array_val, DX_mat_val, PR_mat_val, hosp_array_val, other_mat_val, labels_flatten_val, 
                                    conditions_val], [Y_val, np.random.randn(Y_val.shape[0],1)]], 
                     callbacks=[auccheckpoint, reduce_lr, earlystop], class_weight=class_weight, verbose=2)
     
-    model.load_weights(model_path+'skipgram_temp'+str(job_index)+'.h5')
-    y = model.predict([DX1_array_tst, DX_mat_tst, PR_mat_tst, hosp_array_tst, other_mat_tst, labels_flatten_tst, conditions_tst], 
+model.load_weights(model_path+'skipgram_temp'+str(job_index)+'.h5')
+y = model.predict([DX1_array_tst, DX_mat_tst, PR_mat_tst, hosp_array_tst, other_mat_tst, labels_flatten_tst, conditions_tst], 
                       verbose=0)
-    y_pred = y[0][:, 1]
-    fpr, tpr, _ = roc_curve(y_true, y_pred)
-    roc_auc = auc(fpr, tpr)
-    auc_lst.append(roc_auc)
-    y_pred_lst.append(y_pred)
+y_pred = y[0][:, 1]
+fpr, tpr, _ = roc_curve(y_true, y_pred)
+roc_auc = auc(fpr, tpr)
 
-auc_mean = np.mean(auc_lst)
-y_pred_mat = np.column_stack(y_pred_lst)
 now = datetime.now().strftime('%y_%m_%d_%I_%M_%S')
-y_pred_file = 'output/y_pred_mat'+now+'.npy'
-np.save(y_pred_file, y_pred_mat)
-y_pred_avg = y_pred_mat.mean(axis=1)
-fpr, tpr, _ = roc_curve(y_true, y_pred_avg)
-auc_avg = auc(fpr, tpr)
+y_pred_file = path+'preprocessed/ami/y_pred/y_pred'+now+'.npy'
+np.save(y_pred_file, y_pred)
 with open(result_file.format(job_index), 'a') as f:
-    f.write('{},{},{},{},{},{:.1E},{:.1f},{},{},{},{},{},{:.3f},{},{:.5f},{:.5f},{}\n'.format(model_name, code_embed_dim, hosp_embed_dim, fc_width, md_width, lr1, dropout, batchsize, embed_file, cohort, tst_seed, n_fold, lamb, n_samples, auc_mean, auc_avg, y_pred_file))
+    f.write('{},{},{},{},{},{:.1E},{:.1f},{},{},{},{},{},{:.3f},{},{:.5f},{}\n'.format(model_name, code_embed_dim, hosp_embed_dim, fc_width, md_width, lr1, dropout, batchsize, embed_file, cohort, tst_seed, val_seed, lamb, n_samples, roc_auc, y_pred_file))
