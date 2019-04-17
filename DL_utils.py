d4e1b5d62486fbd7b01b4cafb8ac3e10566473e1
wenshuoliu <wenshuoliu@gmail.com>
2019-04-17 17:06:49 -0400

    UC_colonoscopy: regenerate plots for paper.

--- DL_utils.py
+++ DL_utils.py
@@ -4,7 +4,7 @@ from itertools import cycle
 from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 
-def plot_roc(y_pred, y_true, classes=None, title=None, savefile=None):
+def plot_roc(y_pred, y_true, classes=None, title=None, savefile=None, n_digits=3):
     """This function plot the ROC curve and return the AUC"""
     if len(y_pred.shape)==1:
         y_pred = y_pred.reshape(y_pred.shape+(1,))
@@ -30,8 +30,7 @@ def plot_roc(y_pred, y_true, classes=None, title=None, savefile=None):
 
     for i, color in zip(range(n_classes), colors):
         plt.plot(fpr[i], tpr[i], color=color,
-                 label='ROC curve of {0} (area = {1:0.4f})'
-                 ''.format(legends[i], roc_auc[i]))
+                 label=('ROC curve of {0} (area = {1:0.'+str(n_digits)+'f})').format(legends[i], roc_auc[i]))
     plt.plot([0, 1], [0, 1], 'k--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
